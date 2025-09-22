8ffa8e3c960587364c161d643f7f00d953df0b9a
Студент <@PC65-06>
2025-09-22 14:05:13 +0300

    уйц

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст  ");
+            string text = Console.ReadLine();
+            string[] words = text.Split();
+            int wordCount = words.Length;
+            Console.WriteLine($"Количество слов   {wordCount}");
+        }
+    }
+}
